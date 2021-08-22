fe893ba580537ffb91d604ab1c0a98010b638cea
rinaldideveloper <rinaldi.developer@gmail.com>
2021-08-22 22:08:19 +0700

    master branch

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bwa_cozy/Pages/splash_page.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashPage(),
+    );
+  }
+}
